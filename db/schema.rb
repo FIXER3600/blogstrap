c9bcd6ace0454d4acbd192d00587d75fadd1be6a
FIXER3600 <guilhermecomercial51@gmail.com>
2023-12-19 18:26:08 -0300

    feat:Adicionando listagem de articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_19_034459) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
