751d989760f20cd154bb79689142718cec2cfaf3
FIXER3600 <guilhermecomercial51@gmail.com>
2024-01-05 15:58:15 -0300

    feat:Editando um article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,20 @@ class ArticlesController < ApplicationController
      render :new
    end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title,:body)
